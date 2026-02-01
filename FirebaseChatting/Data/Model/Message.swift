87bb6d483e4febbdf3ff5e717de2897d98582cce
sangjin-hash <sangjin970301@gmail.com>
2026-02-02 00:00:09 +0900

    [FEAT] 그룹 채팅 지원을 위한 모델 및 API 추가

--- FirebaseChatting/Data/Model/Message.swift
+++ FirebaseChatting/Data/Model/Message.swift
@@ -18,6 +18,10 @@ struct Message: Equatable, Sendable, Codable, Identifiable {
     var mediaUrls: [String]           // 미디어 URL 목록
     var createdAt: Date
 
+    // 시스템 메시지용 메타데이터 (나간 사용자 정보)
+    var leftUserId: String?           // 나간 사용자 ID (재초대용)
+    var leftUserNickname: String?     // 나간 사용자 닉네임
+
     init(
         id: String,
         index: Int,
@@ -25,7 +29,9 @@ struct Message: Equatable, Sendable, Codable, Identifiable {
         type: MessageType,
         content: String? = nil,
         mediaUrls: [String] = [],
-        createdAt: Date = Date()
+        createdAt: Date = Date(),
+        leftUserId: String? = nil,
+        leftUserNickname: String? = nil
     ) {
         self.id = id
         self.index = index
@@ -34,6 +40,8 @@ struct Message: Equatable, Sendable, Codable, Identifiable {
         self.content = content
         self.mediaUrls = mediaUrls
         self.createdAt = createdAt
+        self.leftUserId = leftUserId
+        self.leftUserNickname = leftUserNickname
     }
 }
 
